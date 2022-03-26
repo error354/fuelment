a7da34b59cb7735129504cb5f346ad74e967991a
Error354 <error354@disroot.org>
2022-03-26 22:14:54 +0100

    Add transitions on order changing

--- web/src/pages/IndexPage.vue
+++ web/src/pages/IndexPage.vue
@@ -2,12 +2,20 @@
   <q-page>
     <div class="row justify-center q-pr-lg q-py-lg">
       <draggable
-        tag="div"
         :list="vehicles"
         class="list-group full-width row justify-start q-col-gutter-lg"
         item-key="order"
         handle=".handle"
         @change="changeOrder"
+        tag="transition-group"
+        :component-data="{
+          tag: 'div',
+          type: 'transition-group',
+          name: !drag ? 'flip-list' : null,
+        }"
+        v-bind="dragOptions"
+        @start="drag = true"
+        @end="drag = false"
       >
         <template #item="{ element }">
           <div class="col-12 col-md-6 col-lg-4 col-xl-3">
@@ -31,16 +39,37 @@ export default defineComponent({
   setup() {
     const profileStore = useProfileStore();
     const vehicles = ref(profileStore.getVehicles);
+
+    const drag = ref(false);
+    const dragOptions = ref({
+      animation: 200,
+      ghostClass: "ghost",
+    });
     const changeOrder = (changed) => {
       profileStore.updateVehicleOrder(
         changed.moved.element.id,
         changed.moved.newIndex
       );
     };
+
     return {
       vehicles,
       changeOrder,
+      drag,
+      dragOptions,
     };
   },
 });
 </script>
+
+<style>
+.flip-list-move {
+  transition: transform 0.5s;
+}
+.no-move {
+  transition: transform 0s;
+}
+.ghost {
+  opacity: 0.5;
+}
+</style>
